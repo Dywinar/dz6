e2f2bc22af10323b2e037f718528dfb410d9dd01
Dywinar <144463363+Dywinar@users.noreply.github.com>
2023-10-21 20:20:35 +0300

    6.2.py

---
+++ 6.2.py
@@ -0,0 +1,7 @@
+def vivod(ima, famil, othestvo, god_rohd):
+  print(ima,  famil, othestvo, god_rohd,  "зарегистрирован.А")
+ima = input("Ваше имя: ")
+famil = input("Ваша фамилия: ")
+othestvo = input("Ваше отчество: ")
+god_rohd = input("Ваш год рождения: ")
+vivod(ima, famil,othestvo, god_rohd)
