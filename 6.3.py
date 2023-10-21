570359a3215ee67b9b1d86e433dee019c16cbc96
Dywinar <144463363+Dywinar@users.noreply.github.com>
2023-10-21 20:21:24 +0300

    6.3.py

---
+++ 6.3.py
@@ -0,0 +1,11 @@
+def maximum(a, b, c=0):
+  if c == 0:
+    return b if b>= a else a
+  else:
+    if a >= b and a >= b:
+      return a
+    elif b >= a and b>= c:
+      return b
+    else:
+      return c
+maximum(1,3)
