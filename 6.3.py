bd0f2c02525affc943243c942237302a8edf8814
Dywinar <144463363+Dywinar@users.noreply.github.com>
2023-11-08 15:04:14 +0300

    Update 6.3.py

--- 6.3.py
+++ 6.3.py
@@ -1,8 +1,12 @@
 def maximum(a, b, c=0):
-        if a >=b and a >= c:
+  if c == 0:
+    return b if b >= a else a
+  else:
+    if a >=b and a >= c:
             return a
-        if b >=a and b >= c:
+    if b >=a and b >= c:
             return b       
-        if c >=b and c >= a:
+    if c >=b and c >= a:
             return c
-print(maximum(-8,5,87))
+print(maximum(-8,-9))
+# тут если этот пример без условия c ==0, то тогда будет выведенно 0, но зачем он нам, если мы находим максимальное уже среди 2
