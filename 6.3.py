6f9a3a827ea14b3b82061b895a0be92696724d6d
Dywinar <144463363+Dywinar@users.noreply.github.com>
2023-11-08 14:57:16 +0300

    Update 6.3.py

--- 6.3.py
+++ 6.3.py
@@ -1,7 +1,5 @@
 def maximum(a, b, c=0):
-    if  ((a < 0 and b >0) or (a > 0 and b <0)) and c == 0:
-        return a if a > b else b
-    elif a <0 and b <0 and c == 0:
+    if a <0 and b <0 and c == 0:
       return c
     else:
         if a >=b and a >= c:
@@ -10,5 +8,5 @@ def maximum(a, b, c=0):
             return b       
         if c >=b and c >= a:
             return c
-print(maximum(-3,-999,0))
+print(maximum(7,8,-99))
 #исправил и доработал (если 2 отрицательных)
