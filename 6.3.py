3d95216c28980173b6bd81a048c7e8e0e40c2a22
Dywinar <144463363+Dywinar@users.noreply.github.com>
2023-11-08 14:47:54 +0300

    Update 6.3.py

--- 6.3.py
+++ 6.3.py
@@ -1,11 +1,12 @@
 def maximum(a, b, c=0):
-  if c == 0:
-    return b if b>= a else a
-  else:
-    if a >= b and a >= b:
-      return a
-    elif b >= a and b>= c:
-      return b
+    if  (a < 0 or b <0) and c == 0:
+        return a if a > b else b
     else:
-      return c
-maximum(1,3)
+        if a >=b and a >= c:
+            return a
+        if b >=a and b >= c:
+            return b       
+        if c >=b and c >= a:
+            return c
+print(maximum(-3, -548989546,-1))
+#исправил и доработал (если 2 отрицательных)
