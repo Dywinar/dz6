fc25544d540cc4a2814a4a63050c811456a06058
Dywinar <144463363+Dywinar@users.noreply.github.com>
2023-11-08 14:58:38 +0300

    Update 6.3.py

--- 6.3.py
+++ 6.3.py
@@ -1,12 +1,8 @@
 def maximum(a, b, c=0):
-    if a <0 and b <0 and c == 0:
-      return c
-    else:
         if a >=b and a >= c:
             return a
         if b >=a and b >= c:
             return b       
         if c >=b and c >= a:
             return c
-print(maximum(7,8,-99))
-#исправил и доработал (если 2 отрицательных)
+print(maximum(-8,5,87))
